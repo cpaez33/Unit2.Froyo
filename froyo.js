67a4bebf4d38114320152bdf6e07f4c1b7709c82
Christopher Paez <paez.christopher@gmail.com>
2025-04-25 01:19:37 -0400

    Create function that counts froyo flavor and returns a froyo frequency object

--- froyo.js
+++ froyo.js
@@ -4,4 +4,34 @@ const userInputString = prompt(
 
 const froyoFlavors = userInputString.split(",");
 
-console.log(froyoFlavors);
+// console.log(froyoFlavors);
+
+const flavors = {};
+
+// for (let i = 0; i < froyoFlavors.length; i++) {
+//   const flavor = froyoFlavors[i];
+//   console.log(flavor);
+//   if (flavors[flavor] === undefined) {
+//     flavors[flavor] = 1;
+//   } else {
+//     flavors[flavor] += 1;
+//   }
+// }
+// console.log(flavors);
+
+function countFroyoFrequencies(froyoFlavors) {
+  const flavors = {};
+
+  for (let i = 0; i < froyoFlavors.length; i++) {
+    const flavor = froyoFlavors[i];
+    // console.log(flavor);
+    if (flavors[flavor] === undefined) {
+      flavors[flavor] = 1;
+    } else {
+      flavors[flavor] += 1;
+    }
+  }
+  return flavors;
+}
+
+console.log(countFroyoFrequencies(froyoFlavors));
